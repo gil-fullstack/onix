3eadd4b3a83a049050376310f43985b445c8539f
gil-fullstack <gilservicosweb@gmail.com>
2025-08-22 12:05:35 -0300

    prepared to build app with vue pages layouts

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="./assets/bigger_logo.png" class="logo" alt="Vite logo" />
-    </a>
-
-
-  </div>
-<!--  <HelloWorld msg="Vite + Vue" />-->
+  <router-view/>
 </template>
 
 <style scoped>
---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+  <div class="my_main">
+    <a href="#" target="_blank">
+      <img src="../assets/bigger_logo.png" width="90%" class="logo" alt="Vite logo" />
+    </a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.my_main{
+  background-color: #242424;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+  a{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    max-width: 50%;
+  }
+}
+</style>
